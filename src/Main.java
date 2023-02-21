bd9683a8f159f0d6eaf42d0667c33c4cf75cecfa
admin <mmd83@63.ru>
2023-02-21 19:52:52 +0400

    HW lesson12 Stream API

--- src/Main.java
+++ src/Main.java
@@ -27,7 +27,6 @@ public class Main {
                 System.out.println("min=" + min + ", max=" + max);
             }
         };
-
         findMinMax(sourceList.stream(), compareInt, minMaxIntegerConsumer);
 
 
